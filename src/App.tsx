17cbb8b416e0532f14df50750e8379689d0aa63b
jonahhhhhh <jparisi1@madisoncollege.edu>
2023-09-27 12:58:46 -0500

    Added hash router

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,30 @@ import './App.css';
 import Button from '@mui/material/Button';
 import Icon from '@mui/material/Icon';
 import SmartDisplayIcon from '@mui/icons-material/SmartDisplay';
+import {
+  createHashRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <div>Home</div>,
+  },
+  {
+    path: "/setup",
+    element: <div>Setup</div>,
+  },
+  {
+    path: "/play",
+    element: <div>Play</div>,
+  },
+])
 
 function App() {
   return (
     <div className="App">
-      <Button variant="outlined" size="large" startIcon={<SmartDisplayIcon />}> Play Counter Strike</Button>
+      <RouterProvider router={router} />
     </div>
   );
 }
