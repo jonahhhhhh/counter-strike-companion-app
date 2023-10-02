fead1271ce377f2ab830d426e6e214f78a46d9fe
jonahhhhhh <jparisi1@madisoncollege.edu>
2023-10-02 13:40:05 -0500

    using react states to pass data between components

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -11,26 +11,36 @@ import {Home} from "./Home";
 import {Setup} from "./Setup";
 import {Play} from "./Play";
 
-const router = createHashRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/setup",
-    element: <Setup />,
-  },
-  {
-    path: "/play",
-    element: <Play />,
-  },
-]);
-
 const App = () => {
-  return (
-    <div className="App">
-      <RouterProvider router={router} />
-    </div>
+
+  const [num, setNum] = useState(1);
+
+  const router = createHashRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/setup",
+      element: <Setup 
+      num={num}
+      setNum={setNum}
+      />,
+    },
+    {
+      path: "/play",
+      element: <Play />,
+    },
+  ]);
+
+    return (
+        <div className="App">
+        <RouterProvider router={router} />
+        <h3>Games Played: {num}</h3>
+        <h3>Games Won:</h3>
+        <h3>Games Lost:</h3>
+        <h3>W/L Ratio:</h3>
+      </div>
   );
 };
 
--- src/Setup.tsx
+++ src/Setup.tsx
@@ -1,19 +1,32 @@
 import Button from '@mui/material/Button';
+import { FC, useState } from 'react';
 import { useNavigate } from "react-router-dom";
 
-export const Setup = () => {
+interface SetupProps {
+    num: number;
+    setNum: any;
+};
+
+export const Setup: FC<SetupProps> = ({num, setNum}) => {
 
     const navigate = useNavigate();
 
     return (
         <>
-        <h3>Setup a Game of Counter-Strike</h3>
-        <Button
-            variant="outlined"
-            size="large"
-            onClick={() => navigate('/play')}
-        >
-            Start Game!</Button>
+            <h3>
+                Setup a Game of Counter-Strike ({num})
+            </h3>
+            <Button
+                variant="outlined"
+                size="large"
+                onClick={
+                    () => {
+                        setNum(num + 1);
+                        navigate('/play');
+                    }
+                }
+            >
+                Start Game!</Button>
         </>
     );
   };
